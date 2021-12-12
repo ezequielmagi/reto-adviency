81faf43b6b42471c0fb9f6db01c4f17bd63e850e
ezequielmagi <44824950+ezequielmagi@users.noreply.github.com>
2021-12-12 15:08:12 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Regalos</h1>
+      <ul>
+        <li>Medias</li>
+        <li>Calzoncillos</li>
+        <li>Crema de afeitar</li>
+      </ul>
+    </div>
+  );
+}
